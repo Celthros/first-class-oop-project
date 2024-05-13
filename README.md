4f338a74565c8d3019ba01fd4c688442423c9460
tyler <mr.anderson@realtourvision.com>
2024-05-12 21:01:11 -0400

    learned basics on classes and objects, and I'm trying to make a simple shopping cart program. I'm trying to add a product to the cart, but I'm getting an error. Here is my code:

---
+++ README.md
@@ -0,0 +1,16 @@
+# learning more on Javascript Classes and Objects
+## Table of Contents
+## Introduction
+1. define a class
+2. constructor
+3. fields, methods, and properties
+4. multiple constructors
+5. inheritance
+6. instance of
+7. static methods
+8. getters and setters
+9. private fields
+10. private methods
+11. private properties
+12. private constructor
+
---
+++ assets/scripts/app.js
@@ -0,0 +1,191 @@
+class Product {
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+
+class Component {
+  constructor(renderHookId, shouldRendor = true) {
+    this.hookId = renderHookId;
+    if (shouldRendor) {
+      this.rendor();
+    }
+  }
+
+  rendor() {}
+
+  createRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+
+class ShoppingCart extends Component {
+  items = [];
+
+  /**
+   * @param {any[]} value
+   */
+  set cartItems(value) {
+    this.items = value;
+    this.totalOutput.innerHTML = `<h2>Total: \$${this.totalAmount.toFixed(
+      2
+    )}</h2>`;
+  }
+
+  get totalAmount() {
+    const sum = this.items.reduce(
+      (prevValue, curItem) => prevValue + curItem.price,
+      0
+    );
+    return sum;
+  }
+
+  constructor(renderHookId) {
+    super(renderHookId, false);
+    this.orderProducts = () => {
+      console.log("Ordering...");
+      console.log(this.items);
+    };
+    this.rendor();
+  }
+
+  addProduct(product) {
+    const updateItems = [...this.items];
+    updateItems.push(product);
+    this.cartItems = updateItems;
+  }
+
+  rendor() {
+    const cartEl = this.createRootElement("section", "cart");
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    const orderButton = cartEl.querySelector("button");
+    //orderButton.addEventListener("click", () => this.orderProducts());
+    orderButton.addEventListener("click", this.orderProducts);
+    this.totalOutput = cartEl.querySelector("h2");
+  }
+}
+
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false);
+    this.product = product;
+    this.rendor();
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  rendor() {
+    const prodEl = this.createRootElement("li", "product-item");
+    prodEl.innerHTML = `
+      <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}">
+          <div class="product-item__content">
+              <h2>${this.product.title}</h2>
+              <h3>\$${this.product.price}</h3>
+              <p>${this.product.description}</p>
+              <button>Add to cart</button>
+          </div>
+      </div>
+    `;
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
+    return prodEl;
+  }
+}
+
+class ProductList extends Component {
+  #products = [];
+
+  constructor(renderHookId) {
+    super(renderHookId, false);
+    this.rendor();
+    this.fetchProducts();
+  }
+
+  fetchProducts() {
+    this.#products = [
+      new Product(
+        "A Pillow",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/Pillows_on_a_hotel_bed.jpg/679px-Pillows_on_a_hotel_bed.jpg",
+        "A soft pillow",
+        19.99
+      ),
+      new Product(
+        "A Carpet",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Wollteppich_1.jpg/220px-Wollteppich_1.jpg",
+        "A carpet which you might like - or not.",
+        89.99
+      ),
+    ];
+    this.rendorProducts();
+  }
+
+  rendorProducts() {
+    for (const prod of this.#products) {
+      new ProductItem(prod, "prod-list");
+    }
+  }
+
+  rendor() {
+    this.createRootElement("ul", "product-list", [
+      new ElementAttribute("id", "prod-list"),
+    ]);
+    if (this.#products && this.#products > 0) {
+      this.rendorProducts();
+    }
+  }
+}
+
+class Shop extends Component {
+  constructor() {
+    super();
+  }
+
+  rendor() {
+    this.cart = new ShoppingCart("app");
+    new ProductList("app");
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  App.init();
+});
